66a8de1f01320937a8a30fea369f72d34094e7eb
Miguel Melgar [Mac] <miguelhpm@gmail.com>
2020-02-09 13:09:39 -0500

    agregando demo de react native elements

--- src/screens/RNElementsScreen.js
+++ src/screens/RNElementsScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View } from 'react-native';
+import { Header, Card, Icon, ListItem, Button } from 'react-native-elements';
 
 export default class RNElementsScreen extends React.Component {
 
@@ -7,24 +8,76 @@ export default class RNElementsScreen extends React.Component {
         super(props);
       }
 
+    renderHeaderLeftComponent () {
+        return (
+            <Icon
+            name='arrow-back'
+            color='#fff'
+            onPress={() => this.props.navigator.pop()} />
+        )
+      }
+
+    renderBodyComponent () {
+        const list = [
+            {
+            name: 'Amy Farha',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+            subtitle: 'Vice President'
+            },
+            {
+            name: 'Chris Jackson',
+            avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+            subtitle: 'Vice Chairman'
+            }
+        ];
+        return (
+            <View>
+                <Card title="CARD WITH DIVIDER">
+                    {
+                        list.map((l, i) => (
+                        <ListItem
+                            key={i}
+                            leftAvatar={{ source: { uri: l.avatar_url } }}
+                            title={l.name}
+                            subtitle={l.subtitle}
+                            chevron
+                        />
+                        ))
+                    }
+                </Card>
+                <Card title="CARD WITH DIVIDER">
+                    {
+                        list.map((l, i) => (
+                        <ListItem
+                            key={i}
+                            leftAvatar={{ source: { uri: l.avatar_url } }}
+                            title={l.name}
+                            subtitle={l.subtitle}
+                            chevron
+                        />
+                        ))
+                    }
+                </Card>
+            </View>
+        )
+      }
+
     render() {
         return (
-            <View style={{
-                // Transparent background because mask is based off alpha channel.
-                backgroundColor: 'transparent',
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}>
-                <Text
-                style={{
-                    fontSize: 60,
-                    color: 'black',
-                    fontWeight: 'bold'
-                }}
-                >
-                    React Native Elements
-                </Text>
+            <View>
+                <Header
+                    leftComponent={this.renderHeaderLeftComponent()}
+                    centerComponent={{ text: 'Shaman RNElements', style: { color: '#fff' } }}
+                    rightComponent={{ icon: 'menu', color: '#fff' }}
+                    />
+                {this.renderBodyComponent()}
+                <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                    <Icon
+                        reverse
+                        name="plus" type="font-awesome"
+                        color='#5067FF'
+                        onPress={() => console.log('hello')} />
+                </View>
             </View>
         );
     }
